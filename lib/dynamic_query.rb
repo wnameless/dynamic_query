6822c8d17f41a89a28d444c35e12a04ae1508f74
WMW <wnameless@gmail.com>
2013-03-06 12:14:41 +0800

    Add multi_columns_join method

--- lib/dynamic_query.rb
+++ lib/dynamic_query.rb
@@ -2,6 +2,7 @@ require 'dynamic_query/railtie' if defined?(Rails)
 require 'generators/helper_generator' if defined?(Rails)
 require 'dynamic_query/combined_query'
 require 'dynamic_query/validator'
+require 'arel'
 
 module DynamicQuery
   OPERATOR = ['=', '>', '>=', '<', '<=', '!=',
@@ -11,9 +12,44 @@ module DynamicQuery
   def dynamic_query(*models, opt)
     DynamicQueryInstance.new(*models, opt)
   end
+  
+  def multi_columns_join(from, *to_tables)
+    f = Arel::Table.new from.table_name
+    alias_counter = {}
+    table_aliases = {}
+    to_tables.each do |tt|
+      to_table_name = tt.first.table_name
+      t = Arel::Table.new to_table_name
+      table_aliases[to_table_name] ||= []
+      tt.drop(1).each do |mapping|
+        if alias_counter[to_table_name].nil?
+          alias_counter[to_table_name] = 0
+          table_aliases[to_table_name] << t
+        end
+        alias_counter[to_table_name] += 1
+        table_aliases[to_table_name] << Arel::Table.new(to_table_name, :as => "#{to_table_name}_#{alias_counter[to_table_name]}")
+      end
+    end
+    
+    query = f.clone
+    to_tables.each do |tt|
+      to_table_name = tt.first.table_name
+      tt.drop(1).each do |mapping|
+        target = table_aliases[to_table_name].shift
+        if mapping.kind_of? Array
+          query = query.join(target).on( f[mapping.first].eq target[mapping.last] )
+        else
+          query = query.join(target).on( f[mapping].eq target[mapping] )
+        end
+      end
+    end
+    
+    sql = query.project(Arel.sql('*')).to_sql
+    sql[sql.index('INNER JOIN')..-1]
+  end
               
   class DynamicQueryInstance
-    include CombineQuery, Validator
+    include CombinedQuery, Validator
     
     def initialize(*models, opt)
       @reveal_keys = false
